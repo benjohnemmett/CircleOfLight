b8b5d3828d958410aadd7f5e6b4a30311c81e75c
Ben Emmett <ben@FlyerII.attlocal.net>
2022-06-20 22:00:02 -0500

    ReflexProgram game mostly implemented. Lacking score display.

--- ReflexProgram.cpp
+++ ReflexProgram.cpp
@@ -3,63 +3,178 @@
 ReflexProgram::ReflexProgram(LightController *light_controller, ButtonManager *button_manager) {
     this->light_controller_ = light_controller;
     this->button_manager_ = button_manager;
-    this->cycles_per_light_ = 50;
+    this->game_is_running_ = 1;
 }
 
 void ReflexProgram::SetupProgram() {
     this->light_controller_->SetupLights();
     this->button_manager_->SetListener(this);
+    this->state.ChangeToState(ENTRY);
+    this->cycles_per_light_index_ = 2;
+    this->score_ = 0;
+    this->turn_ = 0;
+    this->game_is_running_ = 1;
 }
 
 void ReflexProgram::Update() {
-    uint8_t previous_pin = active_pin_index_;
 
-    if (current_cycle_ >= cycles_per_light_) {
-        if (rotation_is_clockwise_) {
-            if (active_pin_index_ > 1) {
-                active_pin_index_--;
-            } else {
-                active_pin_index_ = 12;
-            }
-        } else {
-            active_pin_index_ += 1;
-            if (active_pin_index_ > 12) {
-                active_pin_index_ = 1;
-            }
-        }
-
-        this->light_controller_->SetLightValue(previous_pin, 0);
-        this->light_controller_->SetLightValue(active_pin_index_, 1);
-        current_cycle_ = 0;
+    switch(this->state.CurrentState()) {
+        case ENTRY:
+            this->HandleEntryUpdate();
+            break;
+        case WAITING_FOR_BTN_PRESS:
+            this->HandleWaitingForButtonPress();
+            break;
+        case SPINNING:
+            this->HandleSpinningUpdate();
+            break;
+        case SCORE_TURN:
+            this->HandleScoreTurnUpdate();
+            break;
+        case DISPLAY_SCORE:
+            this->HandleDisplayScoreUpdate();
+            break;
+        case EXIT:
+            this->HandleExitUpdate();
     }
 
-    current_cycle_++;
+    this->state.Update();
+}
+
+void ReflexProgram::HandleEntryUpdate() {
+    uint16_t ticks = this->state.UpdateTicksInState();
+
+    if (ticks % 64 == 0) {
+        uint8_t light_to_activate = (ticks / 64) + 6;
+        uint8_t other_light;
+        if (light_to_activate < 12) {
+            other_light = 12 - light_to_activate;
+        }
+
+        if (light_to_activate >= 12) {
+            this->light_controller_->ClearAllLights();
+            this->state.ChangeToState(WAITING_FOR_BTN_PRESS);
+        } else {
+            this->light_controller_->ClearAllLights();
+            this->light_controller_->SetLightValue(light_to_activate, 1);
+            this->light_controller_->SetLightValue(other_light, 1);
+        }
+    }
+}
+
+void ReflexProgram::HandleWaitingForButtonPress() {
+    uint16_t ticks = this->state.UpdateTicksInState();
+
+    if (ticks >= GetCyclesPerLight()) {
+        this->toggle_light_state_ = !this->toggle_light_state_;
+        this->light_controller_->SetLightValue(12, this->toggle_light_state_);
+        this->state.ResetTicks();
+    }
+}
+
+void ReflexProgram::HandleSpinningUpdate() {
+    uint16_t ticks = this->state.UpdateTicksInState();
+
+    if (ticks >= GetCyclesPerLight()) {
+        this->light_controller_->SetLightValue(this->active_light_index_, 0);
+        this->IncrementActiveLightIndex();
+        this->light_controller_->SetLightValue(this->active_light_index_, 1);
+        
+        this->state.ResetTicks();
+    }
+}
+
+void ReflexProgram::HandleScoreTurnUpdate() {
+    uint16_t ticks = this->state.UpdateTicksInState();
+
+    if (ticks > 6*256) {
+        this->light_controller_->ClearAllLights();
+        if (this->turn_ < 3) {
+            this->state.ChangeToState(WAITING_FOR_BTN_PRESS);
+        } else {
+            this->state.ChangeToState(DISPLAY_SCORE);
+        }
+    }
+
+    if (ticks % 256 == 0) {
+        this->toggle_light_state_ = !this->toggle_light_state_;
+        this->light_controller_->SetLightValue(this->active_light_index_, this->toggle_light_state_);
+    }
+}
+
+void ReflexProgram::HandleDisplayScoreUpdate() {
+
+    SendStringToUart((char*)"RP::Game over. Score - ");
+    Print((uint16_t)this->score_);
+    SendStringToUart((char*)"\r\n");
+
+    this->state.ChangeToState(EXIT);
+}
+
+void ReflexProgram::HandleExitUpdate() {
+        uint16_t ticks = this->state.UpdateTicksInState();
+
+    if (ticks % 64 == 0) {
+        uint8_t light_to_activate = (ticks / 64) + 1;
+        uint8_t other_light;
+        if (light_to_activate < 12) {
+            other_light = 12 - light_to_activate;
+        }
+
+        if (light_to_activate >= 7) {
+            this->light_controller_->ClearAllLights();
+            this->game_is_running_ = 0;
+        } else {
+            this->light_controller_->ClearAllLights();
+            this->light_controller_->SetLightValue(light_to_activate, 1);
+            this->light_controller_->SetLightValue(other_light, 1);
+        }
+    }
 }
 
 void ReflexProgram::TearDownProgram() {
-
+    this->light_controller_->ClearAllLights();
 }
 
 bool ReflexProgram::IsDoneRunning() {
-    return false;
+    return !this->game_is_running_;
+}
+
+
+void ReflexProgram::PlayButtonPressed() {
+    switch(this->state.CurrentState()) {
+        case WAITING_FOR_BTN_PRESS:
+            this->turn_++;
+            this->light_controller_->ClearAllLights();
+            this->state.ChangeToState(SPINNING);
+            break;
+    }
 }
 
 void ReflexProgram::PlusButtonPressed() {
-    if (this->cycles_per_light_ > this->MIN_CYCLES_PER_LIGHT) {
-        this->cycles_per_light_ -= this->CYCLES_PER_LIGHT_STEP;
+    if (this->cycles_per_light_index_ > 0) {
+        this->cycles_per_light_index_--;
     }
-}
-
-void ReflexProgram::PlayButtonPressed() {
-    rotation_is_clockwise_ = !rotation_is_clockwise_;
 }
 
 void ReflexProgram::MinusButtonPressed() {
-    if (this->cycles_per_light_ < this->MAX_CYCLES_PER_LIGHT) {
-        this->cycles_per_light_ += this->CYCLES_PER_LIGHT_STEP;
+    if (this->cycles_per_light_index_ < this->cycles_per_light_array_length_ - 1) {
+        this->cycles_per_light_index_++;
     }
 }
 
-void ReflexProgram::PlayButtonReleased() {}
+void ReflexProgram::PlayButtonReleased() {
+        switch(this->state.CurrentState()) {
+        case SPINNING:
+            int8_t points = light_score_mapping_[active_light_index_ - 1];
+            this->score_ += points;
+            if (this->score_ < 0) {
+                this->score_ = 0;
+            }
+            this->state.ChangeToState(SCORE_TURN);
+            break;
+    }
+}
+
 void ReflexProgram::PlusButtonReleased() {}
 void ReflexProgram::MinusButtonReleased() {}
--- ReflexProgram.h
+++ ReflexProgram.h
@@ -4,6 +4,48 @@
 #include "IProgram.h"
 #include "LightController.h"
 #include "ButtonManager.h"
+#include "uart.h"
+
+enum ReflexProgramStateEnum {
+    NONE,
+    ENTRY,
+    WAITING_FOR_BTN_PRESS,
+    SPINNING,
+    SCORE_TURN,
+    DISPLAY_SCORE,
+    EXIT
+};
+
+class ReflexProgramState {
+    public:
+        void Update() {
+            this->update_ticks_in_current_state_++;
+        }
+
+        void ChangeToState(ReflexProgramStateEnum new_state) {
+            this->current_state_ = new_state;
+            this->update_ticks_in_current_state_ = 0;
+            SendStringToUart((char*)"RP::Change State ");
+            Print((uint8_t)current_state_);
+            SendStringToUart((char*)"\r\n");
+        }
+
+        ReflexProgramStateEnum CurrentState() {
+            return this->current_state_;
+        }
+
+        uint16_t UpdateTicksInState() {
+            return this->update_ticks_in_current_state_;
+        }
+
+        void ResetTicks() {
+            this->update_ticks_in_current_state_ = 0;
+        }
+
+    private:
+        ReflexProgramStateEnum current_state_ = NONE;
+        uint16_t update_ticks_in_current_state_ = 0;
+};
 
 class ReflexProgram: public IProgram, public IButtonListener {
     public:
@@ -23,14 +65,29 @@ class ReflexProgram: public IProgram, public IButtonListener {
     private:
         LightController *light_controller_;
         ButtonManager *button_manager_;
-        uint8_t current_cycle_;
-        uint8_t cycles_per_light_;
-        uint8_t rotation_is_clockwise_;
-        uint8_t active_pin_index_;
-        uint8_t program_is_done_;
-        const uint8_t MIN_CYCLES_PER_LIGHT = 20;
-        const uint8_t MAX_CYCLES_PER_LIGHT = 80;
-        const uint8_t CYCLES_PER_LIGHT_STEP = 15;
+        ReflexProgramState state;
+        void HandleEntryUpdate();
+        void HandleWaitingForButtonPress();
+        void HandleSpinningUpdate();
+        void HandleScoreTurnUpdate();
+        void HandleDisplayScoreUpdate();
+        void HandleExitUpdate();
+
+        void IncrementActiveLightIndex() { this->active_light_index_ = (this->active_light_index_ % 12) + 1; }
+        uint8_t GetCyclesPerLight() { return this->cycles_per_light_array_[this->cycles_per_light_index_]; }
+
+        uint8_t active_light_index_;
+        const uint8_t cycles_per_light_array_[5] = {40, 60, 80, 100, 120};
+        const uint8_t cycles_per_light_array_length_ = 5;
+        uint8_t cycles_per_light_index_ = 2;
+                                        // 1   2  3    4   5   6    7  8  9  10   11   12
+        int8_t light_score_mapping_[12] = {-10, 5, 25,  5, -1, 50,  -1, 5, 25, 5, -10, 100};
+        int16_t score_;
+        uint8_t turn_;
+
+        uint8_t toggle_light_state_;
+        uint8_t game_is_running_;
+
 };
 
 #endif
